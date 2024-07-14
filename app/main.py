f67ea51c9e8a66d8260abf02e7ee49f412318ef4
ivansnow02 <ivansnow02@gmail.com>
2024-07-14 15:39:31 +0800

    fix：恢复接口鉴权

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 app.include_router(
     langserve.router,
-    # dependencies=[Depends(get_current_user)],
+    dependencies=[Depends(get_current_user)],
 )
 app.include_router(
     chat_with_history.router,
@@ -34,7 +34,7 @@ app.include_router(
 )
 app.include_router(
     ocr.router,
-    # dependencies=[Depends(get_current_user)],
+    dependencies=[Depends(get_current_user)],
 )
 app.include_router(
     user.router,
